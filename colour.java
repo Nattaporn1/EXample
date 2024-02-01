c45dd337be00cd825d5c37ecf6fe15d3bfa62521
Nattaporn Kanjanarujeerat <154008042+Nattaporn1@users.noreply.github.com>
2024-02-01 21:35:02 +0700

    Create colour.java

---
+++ colour.java
@@ -0,0 +1,36 @@
+public class Colour {
+
+    private int red;
+    private int green;
+    private int blue;
+
+    public Colour() {
+        this.red = 0;
+        this.green = 0;
+        this.blue = 0;
+    }
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+   
+}
